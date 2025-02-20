5d46399d3e75bd4145ec652520ad9bf1a89613ae
Shuqy <>
2025-02-20 17:15:30 +0800

    demo

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by yangsq on 2025/2/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
